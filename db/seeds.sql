c03b436a757089d87f8668cec2099b191c1e1a79
Katelynn Cowan <kcowan@twu.edu>
2021-01-15 21:14:16 -0600

    updated files and added partial folder for burger block

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
-INSERT INTO burgers (name, devour) VALUES ('Plain and Dry', true);
-INSERT INTO burgers (name, devour) VALUES ('Bacon Cheeseburger', true);
-INSERT INTO burgers (name, devour) VALUES ('Veggie Burger', true);
-INSERT INTO burgers (name) VALUES ('Llano Poblano');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name, devour) VALUES ('Plain and Dry', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Llano Poblano');

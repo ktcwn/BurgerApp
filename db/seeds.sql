42d9ff6ffdf5ba6ef71a17502c2e097d3f0881c5
Katelynn Cowan <kcowan@twu.edu>
2021-01-15 20:16:21 -0600

    Added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (name, devour) VALUES ('Plain and Dry', true);
+INSERT INTO burgers (name, devour) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (name, devour) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name) VALUES ('Llano Poblano');
